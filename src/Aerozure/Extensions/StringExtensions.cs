81365062a78227bd539916a028c338cca4e6d03e
Sam Vanhoutte <sam.vanhoutte@marchitec.be>
2026-06-02 15:52:28 +0200

    Enable url extensions

--- src/Aerozure/Extensions/StringExtensions.cs
+++ src/Aerozure/Extensions/StringExtensions.cs
@@ -13,5 +13,11 @@ public static class StringExtensions
         return charsToReplace.Aggregate(input,
             (current, c) => current.Replace(c.ToString(), (separator?.ToString() ?? string.Empty)));
     }
-    
+    public static bool IsValidUrl(this string? value, out Uri? uri)
+    {
+        uri = null;
+        if (string.IsNullOrEmpty(value)) return false;
+        return Uri.TryCreate(value, UriKind.Absolute, out uri)
+               && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps);
+    }
 }
